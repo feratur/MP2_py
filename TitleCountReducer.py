f1de350250378872d3b940ef557f96cd64f58cd2
feratur <feraturdev@gmail.com>
2019-03-04 01:25:43 +0300

    Update TitleCountReducer.py

--- TitleCountReducer.py
+++ TitleCountReducer.py
@@ -4,8 +4,12 @@ import sys
 
 #TODO
 
+result = {}
+
 # input comes from STDIN
 for line in sys.stdin:
-    # TODO
+    word, count = line.strip().split('\t')
+    result[word] = result.get(word, 0) + int(count)
 
-# TODO
+for word, count in result.items():
+    print("%s\t%i" % (word, count))
