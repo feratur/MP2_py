9029b137b6830435240b5db7a5036e67d596d0ea
feratur <feraturdev@gmail.com>
2019-03-04 01:39:00 +0300

    Update TopTitlesReducer.py

--- TopTitlesReducer.py
+++ TopTitlesReducer.py
@@ -2,7 +2,11 @@
 import sys
 
 
+word_counts = {}
 
-# input comes from STDIN
 for line in sys.stdin:
-    # TODO
+    word, count = line.strip().split('\t')
+    word_counts[word] = int(count)
+    
+for word, count in sorted(word_counts.items(), key=lambda kvp: (-kvp[1], kvp[0]))[:10]:
+    print("%s\t%i" % (word, count))
