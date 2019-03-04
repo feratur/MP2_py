6cb033d2cd77f7723d83f0023a88391a082d69d9
feratur <feraturdev@gmail.com>
2019-03-04 03:13:00 +0300

    Update TopTitleStatisticsReducer.py

--- TopTitleStatisticsReducer.py
+++ TopTitleStatisticsReducer.py
@@ -2,10 +2,25 @@
 import sys
 
 
-
-#TODO
+values = []
+min_val = 99999
+max_val = 0
 
 for line in sys.stdin:
-    # TODO
+    word, count = line.strip().split('\t')
+    val = int(count)
+    values.append(val)
+    if val < min_val:
+        min_val = val
+    if val > max_val:
+        max_val = val
 
-#TODO
+total = sum(values)        
+mean = total / len(values)
+variance = sum([(v - mean)**2 for v in values]) / len(values)
+
+print('Mean\t%i' % mean)
+print('Sum\t%i' % total)
+print('Min\t%i' % min_val)
+print('Max\t%i' % max_val)
+print('Var\t%i' % variance)
