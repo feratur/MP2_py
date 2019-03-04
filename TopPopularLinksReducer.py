bbc6c32bc7b3a40fda5e2648d1751388ed07c3b5
feratur <feraturdev@gmail.com>
2019-03-04 03:46:39 +0300

    Update TopPopularLinksReducer.py

--- TopPopularLinksReducer.py
+++ TopPopularLinksReducer.py
@@ -2,8 +2,13 @@
 import sys
 
 
+word_counts = {}
 
-# input comes from STDIN
 for line in sys.stdin:
-    # TODO
-
+    word, count = line.strip().split('\t')
+    word_counts[word] = int(count)
+    
+result_items = sorted(word_counts.items(), key=lambda kvp:(kvp[1], kvp[0]), reverse=True)[:10]
+    
+for word, count in sorted(result_items, key=lambda kvp:kvp[0]):
+    print("%s\t%i" % (word, count))
