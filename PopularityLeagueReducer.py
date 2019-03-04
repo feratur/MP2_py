1f2ad0b0dabc8da5e7df5ca9306b400c9b7b0d38
feratur <feraturdev@gmail.com>
2019-03-04 04:14:50 +0300

    Update PopularityLeagueReducer.py

--- PopularityLeagueReducer.py
+++ PopularityLeagueReducer.py
@@ -1,10 +1,19 @@
 #!/usr/bin/env python
 import sys
-#TODO
 
-# input comes from STDIN
+link_counts = {}
+
 for line in sys.stdin:
-    # TODO
+    link, count = line.strip().split('\t')
+    link_counts[link] = int(count)
 
-
-#TODO
+result = {}
+for k1, v1 in link_counts.items():
+    rank = 0
+    for k2, v2 in link_counts.items():
+        if v1 > v2:
+            rank += 1
+    result[k1] = rank
+    
+for link, count in sorted(result.items(), reverse=True):
+    print('%s\t%s' % (link, count))
