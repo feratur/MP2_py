0c3a8bca8bfaab7124434bad8d42c593fb50449f
feratur <feraturdev@gmail.com>
2019-03-04 03:48:23 +0300

    Update OrphanPagesMapper.py

--- OrphanPagesMapper.py
+++ OrphanPagesMapper.py
@@ -3,7 +3,7 @@ import sys
 
 
 for line in sys.stdin:
-  link_from, links_to = line.strip().split(':')
-  print('link_from\t%s' % (link_from.strip()))
-  for link_to in links_to.strip().split():
-      print('link_to\t%s' % (link_to))
+    link_from, links_to = line.strip().split(':')
+    print('link_from\t%s' % (link_from.strip()))
+    for link_to in links_to.strip().split():
+        print('link_to\t%s' % (link_to))
