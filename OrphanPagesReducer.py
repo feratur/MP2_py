7aef4a608d3eed0e60f1d7fe2fa269772ce9b7d0
feratur <feraturdev@gmail.com>
2019-03-04 03:28:36 +0300

    Update OrphanPagesReducer.py

--- OrphanPagesReducer.py
+++ OrphanPagesReducer.py
@@ -2,10 +2,15 @@
 import sys
 
 
-#TODO
-
+links = { 'link_from': [], 'link_to': [] }
 
 for line in sys.stdin:
-  # TODO
+    link_type, link_value = line.strip().split('\t')
+    links[link_type].append(link_value)
+  
+link_to_set = set(links['link_to'])
+orphan_links = sorted([link for link in links['link_from'] if not link in link_to_set])
 
-#TODO
+for link in orphan_links:
+    print(link)
+
