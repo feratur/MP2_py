86865a88eb391dcf560d23e5aa3a743d648fa9bd
feratur <feraturdev@gmail.com>
2019-03-04 03:51:08 +0300

    Update LinkCountReducer.py

--- LinkCountReducer.py
+++ LinkCountReducer.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python
 import sys
 
-#TODO
+result = {}
 
 # input comes from STDIN
 for line in sys.stdin:
-    # TODO
+    word, count = line.strip().split('\t')
+    result[word] = result.get(word, 0) + int(count)
 
-# TODO
+for word, count in result.items():
+    print("%s\t%i" % (word, count))
