5a9fbf65227c0819673e18af84e94534f4281bd9
feratur <feraturdev@gmail.com>
2019-03-04 04:02:59 +0300

    Update PopularityLeagueMapper.py

--- PopularityLeagueMapper.py
+++ PopularityLeagueMapper.py
@@ -6,13 +6,9 @@ leaguePath = sys.argv[1]
 
 
 with open(leaguePath) as f:
-	links = set([line.strip() for line in f.readlines()])
-
-
-
-
+    links = set([line.strip() for line in f.readlines()])
 
 for line in sys.stdin:
-	link, count = line.strip().split('\t')
+    link, count = line.strip().split('\t')
     if link in links:
         print('%s\t%s' % (link, count))
